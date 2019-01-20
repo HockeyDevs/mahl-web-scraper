b1b0f96f8858f178e945396964252c219b8b2e90
Nick Vanselow <nick@nickvanselow.com>
2019-01-20 10:38:14 -0500

    Generate Express Backend

---
+++ mahl-web-scraper-backend/app.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const helmet = require('helmet')
+
+const indexRouter = require('./routes/index')
+
+const app = express()
+
+app.use(helmet())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+
+module.exports = app
